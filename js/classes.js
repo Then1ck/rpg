ef59dbc05d26c8ed5aa1a08716a341a627d3cee6
Then1ck <nickymarcellinokwan@gmail.com>
2026-09-22 12:03:49 +0700

    make classes

---
+++ js/classes.js
@@ -0,0 +1,60 @@
+const Classes_DB = [
+    {
+        id: 'knight',
+        name: 'Knight',
+        desc: 'the Knight is a good all rounded unit who is decent in any team, for his ability to deal good damage and tank a bit of hits.'
+    }, {
+        id: 'mage',
+        name: 'Mage',
+        desc: 'the Mage can casts spells and grant a lot of effects, such that they can either be a great dps or be a great support for the utility of a kit made for a specific purpose.'
+    }, {
+        id: 'guardian',
+        name: 'Guardian',
+        desc: 'the Guardian carries a giant shield and is a heavy tank who can take a ton of hits for allies before going down.'
+    }, {
+        id: 'healer',
+        name: 'Healer',
+        desc: 'the Healer is an amazing supporter for the team, granting advantageous buffs and heals for all allies. Though, a Healer may not carry a team alone.'
+    }, {
+        id: 'thief',
+        name: 'Thief',
+        desc: 'the Thief is a tricky class who has a lot of hidden cards up their sleeves. They can fulfil utility roles, or act as a hidden ace for the team in need.'
+    }, {
+        id: 'barbarian',
+        name: 'Barbarian',
+        desc: 'the Barbarian is a unit who grows in power along with the battle for the buffs they do to themselves, or the buffs they gain through passive means.'
+    }, {
+        id: 'ranger',
+        name: 'Ranger',
+        desc: 'the Ranger is an amazing damage dealer who hits hard from the backlines behind cover. Though when they are vulnerable, the damage they make take is quite lethal.'
+    }, {
+        id: 'beast_tamer',
+        name: 'Beast Tamer',
+        desc: 'the Beast Tamer hangs from the back and sets up traps or summons beasts that they can summon. However, they can be quite vulnerable when alone with no setups.'
+    }, {
+        id: 'bard',
+        name: 'Bard',
+        desc: 'the Bard specializes on buffs and debuffs for allies and opponents. They may not do a lot for damage numbers, but they have a ton of other useful utilities.'
+    }, {
+        id: 'martial_artist',
+        name: 'Martial Artist',
+        desc: 'the Martial Artist hinges on patience and counter attacks. They can block powerful damage, and dish back even stronger attacks.'
+    }, {
+        id: 'paladin',
+        name: 'Paladin',
+        desc: 'the Paladin can not only tank a few hits, but also provide buffs and heals for their allies. Acting as a convenient double role.'
+    }, {
+        id: 'dark_mage',
+        name: 'Dark Mage',
+        desc: 'the Dark Mage can cast powerful spells with a varied amount of usages, yet it will cast a heavy toll on the user.'
+    }, {
+        id: 'vampire',
+        name: 'Vampire',
+        desc: 'the Vampire can take a lot more damage than they might seem at first, and that is proportionate to the damage they deal.'
+    }, 
+    
+    
+    {
+        id: 'dummy'
+    }, 
+]
