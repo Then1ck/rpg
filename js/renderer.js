dfd86b86c7e50e9b43a253207cd265e8d5706480
Then1ck <nickymarcellinokwan@gmail.com>
2026-09-22 09:37:25 +0700

    rough text

---
+++ js/renderer.js
@@ -0,0 +1,26 @@
+const Renderer = {
+    textQueue: [],
+    curText: null,
+
+    makeTextBox(strings) {
+        const tB = document.createElement('textarea');
+        tB.innerText = strings;
+        this.curText = tB;
+
+        document.body.appendChild(tB);
+    },
+
+    addTextBox(strings) {
+        this.textQueue.push(strings);
+    },
+
+    nextText() {
+        if(this.curText){
+            this.curText.remove();
+        }
+
+        if(this.textQueue.length > 0){
+            this.makeTextBox(this.textQueue.pop());
+        }
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+const MainEngine = {
+    startGame() {
+        Renderer.addTextBox("Test Text");
+
+        Renderer.nextText();
+    }
+}
+
+MainEngine.startGame();
